792c0be803eb2b48d10cb58a7710abbdc09c03fd
Konstantin <konstantin_danos@yahoo.de>
2025-10-24 16:10:44 +0300

    completed exercise

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import morgan from 'morgan';
 import mongoose from 'mongoose';
 import 'dotenv/config';
+import methodOverride from 'method-override';
 
 import Dogs from './models/dogs.js'
 
@@ -14,7 +15,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.urlencoded())
 app.use(express.static('public'));
-
+app.use(methodOverride('_method'))
 
 //Routes
 //Allow access to the landing page
@@ -46,6 +47,25 @@ app.get('/dogs/:dogId', async (req, res) => {
     res.render('dogs/show.ejs', {myDog})
 })
 
+//Delete a dog from the database
+app.delete('/dogs/:dogId', async (req, res) =>{
+    await Dogs.findByIdAndDelete(req.params.dogId)
+    console.log('have deleted the dog')
+    res.redirect('/dogs/index');
+})
+
+//Render an edit page
+app.get ('/dogs/:dogId/edit', async (req, res) => {
+    console.log('the dog id is ', req.params.dogId)
+    const foundDog = await Dogs.findById(req.params.dogId);
+    res.render('dogs/edit.ejs',{myDog:foundDog});
+})
+//Edit a dog
+app.put('/dogs/:dogId', async (req, res) => {
+    const myDog = await Dogs.findByIdAndUpdate (req.params.dogId,req.body)
+    // res.send('some text')
+    res.redirect('/dogs/index')
+})
 
 //Connections
 const myLink = process.env.MONGOOSEDB_URI
